2ade6b0701418c79a62e208ea53c1380262c0b8b
ishaanasthana18 <91898798+ishaanasthana18@users.noreply.github.com>
2024-01-21 21:20:35 +0530

    Added page3

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ crsr.style.left = dets.x+"px"
 crsr.style.top = dets.y+"px"})
 
 
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   duration: 0.5,
@@ -30,3 +31,63 @@ gsap.to("#main", {
     },
 
 });
+
+gsap.from("#about-us, #about-us-in, #about-us-img",{
+  y:50,
+  opacity: 0,
+  duration: 1,
+  stragger: 0.4,
+  scrollTrigger:{
+    trigger: "#about-us",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end:"top  55%",
+    scrub: 3
+
+
+  }
+
+})
+
+gsap.from(".card",{
+  scale: 0.8,
+  opacity: 0,
+  duration: 0.5,
+  stragger: 1,
+  scrollTrigger:{
+    trigger: ".card",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end:"top  65%",
+    scrub: 3
+
+
+  }
+
+})
+
+gsap.from("#colon1",{
+  y:-70,
+  x: -70,
+  scrollTrigger: {
+    trigger:"#colon1",
+    scroller: "body",
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  }
+})
+
+gsap.from("#colon2",{
+  y:70,
+  x:70,
+  scrollTrigger: {
+    trigger:"#colon1",
+    scroller: "body",
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 4,
+  }
+})
